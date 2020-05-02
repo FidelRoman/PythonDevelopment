807418c3a2dc21a647bab9d6788e7b446d7fada6
Fidel <fidel.roman@pucp.pe>
2020-05-01 21:35:55 -0500

    Python Development

---
+++ Ex_Files_Learning_Python/Exercise Files/Ch2/helloworld_start.py
@@ -0,0 +1,12 @@
+#
+# Example file for HelloWorld
+#
+
+def main():
+  print("hello world")
+
+# Code for execute function main:
+# The file is executed as a program
+if __name__ == "__main__":
+      main()
+    
